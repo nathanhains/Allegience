c9d3fe10bef0f95f195f4faf99484b0b545d0334
nathan hains <nathanhains14@yahoo.com>
2021-04-09 23:17:42 -0400

    Add logout feature to destroy current user session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@ class ApplicationController < ActionController::Base
     def home
     end
 
-    def current_user
-        if session[:current_user_id]
-            @current_user = User.find(session[:current_user_id])
+    private
+        def current_user
+            if session[:current_user_id]
+                @current_user = User.find(session[:current_user_id])
+            end
         end
-    end
+
+        def login(user)
+            session[:current_user_id] = @user.id
+        end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,15 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
-            session[:current_user_id] = @user.id
+            login(@user)
             redirect_to "/"
         else
             redirect_to "/login", :notice => "Can't find the user."
         end
+    end
+
+    def destroy
+        reset_session
+        redirect_to "/"
     end
 end
