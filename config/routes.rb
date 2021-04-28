e6980c6edc9fdbcf11764addbe3f4e013efea180
nathan hains <nathanhains14@yahoo.com>
2021-04-28 19:58:35 -0400

    Add routes for model controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get '/register', to: 'users#new'
-  post '/users', to: 'users#create'
-  get '/profile', to: 'users#show'
+  resources :users do
+    get :allegience
+  end
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-
   get '/logout', to: 'sessions#destroy'
+
+  get '/roster', to: 'characters#index'
+  get '/selection', to: 'characters#create'
+
   root 'application#home'
 
   resources :jobs
+  resources :civilian_factions do
+    get :requests
+    get :accept
+  end
 
 end
