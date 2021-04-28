7b78edacd256cad1578b15c20c36e9b5250f169f
nathan hains <nathanhains14@yahoo.com>
2021-04-28 19:38:24 -0400

    Add user functionality to contain allegience, default alter_ego & avatars.

--- db/migrate/20210409154519_create_users.rb
+++ db/migrate/20210409154519_create_users.rb
@@ -4,12 +4,9 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :username
       t.string :email
       t.string :password_digest
+      t.string :avatar
       t.string :allegience, default: "Civilian"
       t.string :alter_ego, default: "N/A"
-      t.string :power, default: "N/A"
-      t.integer :civilian_rank, default: 0
-      t.integer :powered_rank, default: 0 
-
       t.timestamps
     end
   end
