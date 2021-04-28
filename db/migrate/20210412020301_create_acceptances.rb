a1691d9655e8969ddfa352a85693a5b1442722e3
nathan hains <nathanhains14@yahoo.com>
2021-04-28 19:41:50 -0400

    Create acceptance model to hold job acceptance to reward creators and job acceptors.

--- db/migrate/20210412020301_create_acceptances.rb
+++ db/migrate/20210412020301_create_acceptances.rb
@@ -3,7 +3,8 @@ class CreateAcceptances < ActiveRecord::Migration[6.1]
     create_table :acceptances do |t|
       t.integer :job_response_id
       t.integer :responder_id
-
+      t.integer :heroization_id
+      t.integer :villainization_id
       t.timestamps
     end
   end
