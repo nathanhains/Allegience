56c6627469c40102e1c4ca8dcf682c9032ee21a3
nathan hains <nathanhains14@yahoo.com>
2021-04-28 19:36:10 -0400

    Add helper methods to run before certain controller actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :current_user
-
+    before_action :go_home, only: :home
     def home
     end
 
@@ -8,7 +8,13 @@ class ApplicationController < ActionController::Base
         def current_user
             if session[:current_user_id]
                 #memoization
-                @current_user ||= User.find(session[:current_user_id])
+                @current_user ||= User.find_by_id(session[:current_user_id])
+            end
+        end
+
+        def check_authorization
+            unless current_user.id == params[:id].to_i
+                redirect_to root_path
             end
         end
 
@@ -23,6 +29,18 @@ class ApplicationController < ActionController::Base
         def authentication_required
             if !logged_in?
                 redirect_to "/login"
+            end
+        end
+
+        def maximum_characters
+            if (current_user.heros.count+current_user.villains.count>=4)
+                redirect_to profile_path(current_user)
+            end
+        end
+
+        def go_home
+            if logged_in?
+                redirect_to user_path(current_user)
             end
         end
 end
