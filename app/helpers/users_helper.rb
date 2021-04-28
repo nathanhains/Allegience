bcc51eb888e9c69025288849efb414645d78d8b4
nathan hains <nathanhains14@yahoo.com>
2021-04-28 19:57:01 -0400

    Allow profile picture additions through carrier wave and minimagick.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+    def avatar_for(user, options = { size: 70})
+        size = options[:size]
+        if user.avatar?
+            # width in place of size
+            image_tag user.avatar.url(:thumb), size: size, class: 'avatar-image'
+        else
+            image_tag "Unknown.png", size: size, class: 'avatar-image'
+        end
+    end
 end
