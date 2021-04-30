b389ef77aeee3ee36d24a683b8c0de60057c7f21
nathan hains <nathanhains14@yahoo.com>
2021-04-29 20:54:48 -0400

    Implement OmniAuth for user within the appropriate controllers and views.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,4 +18,27 @@ class SessionsController < ApplicationController
         reset_session
         redirect_to "/"
     end
+
+    def omniauth
+        user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider:request.env['omniauth.auth'][:provider]) do |u|
+           create_omni_user(u)
+        end
+        if user.valid?
+            login(user)
+            redirect_to "/login"
+        else
+            redirect_to login_path
+        end
+    end
+
+    private
+
+    def create_omni_user(u)
+        u.username = request.env['omniauth.auth'][:info][:first_name]
+        u.alter_ego = request.env['omniauth.auth'][:info][:first_name]
+        u.email = request.env['omniauth.auth'][:info][:email]
+        u.password = SecureRandom.hex(15)
+        u.avatar = File.open('/Users/nathanhains/Development/code/phase3/allegience/allegience/app/assets/images/Unknown.png')
+        u.civilian = Civilian.create(user_id: u.id, name: u.alter_ego, avatar: u.avatar)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
   resources :users do
+    resources :jobs
     get :allegience
   end
+
+  resources :jobs
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
 
   get '/roster', to: 'characters#index'
   get '/selection', to: 'characters#create'
 
   root 'application#home'
 
-  resources :jobs
+  
   resources :civilian_factions do
     get :requests
     get :accept
