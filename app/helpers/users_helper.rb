6a48b20652d0d7fb46ff9570e2d8fb986ebf8be2
nathan hains <nathanhains14@yahoo.com>
2021-05-02 18:11:01 -0400

    Add functionality for circling out user avatars, add scope method per project requirements.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,34 @@ module UsersHelper
             image_tag "Unknown.png", size: size, class: 'avatar-image'
         end
     end
+    
+    def avatar_civ(user, options = { size: 70})
+        size = options[:size]
+        if user.avatar?
+            # width in place of size
+            image_tag user.avatar.url(:thumb), size: size, class: 'avatar-civ'
+        else
+            image_tag "Unknown.png", size: size, class: 'avatar-image'
+        end
+    end
+
+    def avatar_hero(user, options = { size: 70})
+        size = options[:size]
+        if user.avatar?
+            # width in place of size
+            image_tag user.avatar.url(:thumb), size: size, class: 'avatar-hero'
+        else
+            image_tag "Unknown.png", size: size, class: 'avatar-image'
+        end
+    end
+
+    def avatar_villain(user, options = { size: 70})
+        size = options[:size]
+        if user.avatar?
+            # width in place of size
+            image_tag user.avatar.url(:thumb), size: size, class: 'avatar-villain'
+        else
+            image_tag "Unknown.png", size: size, class: 'avatar-image'
+        end
+    end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  scope :first_eight_except, ->(set){
+    all.limit(8).where.not(set)
+  }
+
 end
