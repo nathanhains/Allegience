d29ed6fee7ad514d8127fba68753b064e4319395
nathan hains <nathanhains14@yahoo.com>
2021-05-05 15:45:47 -0400

    Modified seed data for story creation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#starter hero
 captains_power = Power.create(name: "Super Soldier")
+captain_america = Hero.create(name: "Captain America")
+captain_america.power = captains_power
+File.open('/Users/nathanhains/Development/code/phase3/allegience/allegience/app/assets/images/marvel-clip-art-free-free-clipart-images.jpeg') do |f|
+    captain_america.avatar = f
+  end
+captain_america.save
+
+#story
+power_savior = Power.create(name: "Power of the Savior")
+power_conquerer = Power.create(name: "Power of the Conquerer")
+divinitys_past = Power.create(name: "Divinity's Past")
+sins_future = Power.create(name: "Sin's Future")
+heros_wrath = Power.create(name: "Hero's Wrath")
+evils_brutality = Power.create(name: "Evil's Brutality")
+heart_of_gold = Power.create(name: "Heart of Gold")
+shadow_of_absolution= Power.create(name: "Shadow of Absolution")
+nobles_armor= Power.create(name: "Noble's Armor")
+tyrannys_solitude = Power.create(name: "Tyranny's Solitude")
+ressurection = Power.create(name: "Ressurection")
+eternal_infamy = Power.create(name: "Eternal Infamy")
+powers_array = [power_savior, power_conquerer, divinitys_past, sins_future, heros_wrath, evils_brutality, heart_of_gold, shadow_of_absolution, nobles_armor, tyrannys_solitude, ressurection, eternal_infamy]
+starter = User.create(username: "Allegiance", email:"nathanhains14@yahoo.com", password: "dontguess")
+starter.civilian = Civilian.create(user_id: starter.id, name: starter.alter_ego, avatar: starter.avatar)
+heroization = Heroization.create(user_id: starter.id, hero_id: 1)
+starter.save
+
+#saving all powers for later use
+powers_array.each do |power|
+  heroization.powers << power
+end
+
+tutorial_1 = Job.create(requestor_user_id: starter.id, title: "Tutorial: Civilians", description: "Welcome to Allegiance!
+  Civilians - 
+  Upon creation, you were equipped with a civilian character which you will be granted access to create side missions for the Heroes and Villains of the world! At level 2, you will be able to join Civilian Factions where you can interact with other civilians! To level up, civilians earn 5 points per side mission post. Post some up now!", requirement: 0, reward: 5)
+tutorial_2 = Job.create(requestor_user_id: starter.id, title: "Tutorial: Powered", description: "Powered-
+  Selecting a hero/villain allows users to defend/ wreck havoc in the world! Simply put, you will be able to save/conquer other civilian's side missions. Each Hero/Villain is equipped with a default power. As you complete the story mode, you will gain new powers for your individual powered characters. To meet requirements for Story Chapters/ Side Missions, you will have to increase your powered rank which you do so by completing missions that match your level. At level 3, you will be able to join Hero/ Villain factions which you will be able to interact with other powered individuals! Follow through to the end of the story mode to collect all the powers! Have Fun!", requirement: 0, reward: 5)
+
+chapter_1 = Job.create(requestor_user_id: starter.id, title: "Chapter 1", description: "The world has split in two. The ongoing battle for supremecy between Heroes and Villains has reached an all time high of destruction. Among the chaos, a light has shone through to the islands of San-Pier Pacific calling for your name. Will you accept the call?", requirement: 1, reward: 5, power_reward_hero: power_savior.name, power_reward_villain: power_conquerer.name)
+chapter_2 = Job.create(requestor_user_id: starter.id, title: "Chapter 2", description: "Upon arrival, the light of San-Pier Pacific grew ever-bright. Sifting through the palm trees, you learn the object of which the light shines is but an artifiact who's presence casts a feeling of finality. This artifact's true nature is unknown to you, yet, you feel a drive to take possession, releasing this mystical metal from it's constraints. You ponder the thought.", requirement: 2, reward: 5, power_reward_hero: divinitys_past.name, power_reward_villain: sins_future.name)
+chapter_3 = Job.create(requestor_user_id: starter.id, title: "Chapter 3", description: "You awake to a window, detailing a cloudey Summer day. Familiarity overwhelms your body as the realization that your apartment, long destroyed, surrounds you. The daily news reports that the current date is 5 years previous. The day it all began. A sesation takes you out of the initial shock to reveal the object of which called upon your name, has infused within the palm of your hand! Looking up you hear the news reporter reveal the source of the blackouts has been identified. Recalling the events soon to unfold, you collect yourself to determine the next coure of action.", requirement: 3, reward: 5, power_reward_hero: heros_wrath.name, power_reward_villain: evils_brutality.name)
+chapter_4 = Job.create(requestor_user_id: starter.id, title: "Chapter 4", description: "Suited up and in pursuite, the thoughts of loved one's since passed, buildings reformed and the fate of the world are put on pause as you are knocked off course. Time fractions into slow, methodical fragments. You take notice of your heartbeat, now non existent along with any surrounding movement when suddenly the projectile comes into view. Now split in two, one fragment is heading straight for a family of two while the other, an entrusted Nowgo bank. You attempt to move as time suddenly shifts back into motion, speeding up like cogs in a machine. Time releases you of your constraints to make your move.", requirement: 5, reward: 5, power_reward_hero:heart_of_gold.name , power_reward_villain: shadow_of_absolution.name)
+chapter_5 = Job.create(requestor_user_id: starter.id, title: "Chapter 5", description: "The source of the projcetile looms over the sky, casting a shadow across the town. A voice then echoes and ricochets between your temples calling to return the artifact you have stolen. Your hand begins to resonate and glow as you take balance, your boding forming stance like muscle memory. Projectiles are hurdling towards you like a mist of shards as you aim your palm vertically towards the figure. You pause to contemplate the reality of the situation.", requirement: 7, reward: 5, power_reward_hero: nobles_armor.name, power_reward_villain: tyrannys_solitude.name)
+chapter_6 = Job.create(requestor_user_id: starter.id, title: "Chapter 6", description: "Helicopter thuds, screams of terror, and police sirens all ring through the air like a harmonious nightmare. Lights flicker as the sun fleets and the building surrounded by military, pulsates with energy. Time slows again but your movement remains. Recollection of the crater flashes to the front of your mind only to retreat back to the depths again. The corridors of the building seem to guide you as the emptied labs pass you by. Silence. Darkness. A single light shines in the middle of a highly guarded cage. A mirror image of the object within your palm sits wihtin,  radiating with energy despite the crushing silence of time paused. You get a familiar feeling of finality but this time you finally understand why. With your newfound powers, you have the choice to absorb the blast, sacrificing yourself for the longevity of humanity, or turn your back and seize the opportunities of foresight. The artifact pulsates as you open the cage.", requirement: 9, reward: 5, power_reward_hero: ressurection.name, power_reward_villain:eternal_infamy.name)
+
+#roster
 irons_power = Power.create(name: "Powered Armor")
 incredibles_power = Power.create(name: "Super Strength")
 widows_power = Power.create(name: "Martial Arts")
@@ -22,12 +68,6 @@ jokers_power = Power.create(name: "Insanity")
 lexs_power = Power.create(name: "Warsuit")
 poisons_power = Power.create(name: "Deadly Touch")
 
-captain_america = Hero.create(name: "Captain America")
-captain_america.power = captains_power
-File.open('/Users/nathanhains/Development/code/phase3/allegience/allegience/app/assets/images/marvel-clip-art-free-free-clipart-images.jpeg') do |f|
-    captain_america.avatar = f
-  end
-captain_america.save
 
 iron_man = Hero.create(name: "Iron Man")
 iron_man.power = irons_power
